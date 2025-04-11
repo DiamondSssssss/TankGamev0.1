96744d011b4f26b6ff57da8166fa5b84a94424d6
DiamondSssssss <trongtri140604@gmail.com>
2025-04-11 13:48:50 +0700

    level design

--- core/src/main/java/com/mygdx/tankgame/levels/Level1Screen.java
+++ core/src/main/java/com/mygdx/tankgame/levels/Level1Screen.java
@@ -1,9 +1,12 @@
 package com.mygdx.tankgame.levels;
 
-import com.mygdx.tankgame.playertank.PlayerTank;
-import com.mygdx.tankgame.UpgradeScreen;
-import com.mygdx.tankgame.enemies.EnemyTank;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.tankgame.TankGame;
+import com.mygdx.tankgame.UpgradeScreen;
+import com.mygdx.tankgame.buildstuff.Wall;
+import com.mygdx.tankgame.enemies.EnemyTank;
+import com.mygdx.tankgame.playertank.PlayerTank;
 
 public class Level1Screen extends LevelScreen {
     public Level1Screen(TankGame game, PlayerTank playerTank) {
@@ -12,12 +15,23 @@ public class Level1Screen extends LevelScreen {
 
     @Override
     protected void setupLevel() {
+        // Load map data from JSON
+        LevelMapLoader.MapData mapData = LevelMapLoader.load("map_level1.json");
 
-        enemies.add(new EnemyTank(400, 400, playerTank,bullets)); // One enemy
+        // Load background texture
+        backgroundTexture = new Texture(Gdx.files.internal(mapData.background));
+
+        // Add all walls and obstacles
+        for (Wall wall : mapData.walls) {
+            addWall(wall);
+        }
+
+        // Add enemies manually for now (can also be added to JSON later if needed)
+        enemies.add(new EnemyTank(400, 400, playerTank, bullets));
     }
 
     @Override
     protected void goToUpgradeScreen() {
-        game.setScreen(new UpgradeScreen(game, playerTank, 1)); // Go to UpgradeScreen before Level 2
+        game.setScreen(new UpgradeScreen(game, playerTank, 1));
     }
 }
